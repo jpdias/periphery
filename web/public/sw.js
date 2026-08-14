cd436fe46a79b754e7a35ca306039dfd51500bbd
jpdias <jpdias@users.noreply.github.com>
2026-08-14 14:12:33 +0100

    web/server: fix widget visibility, reservoirs 403 headers, drag overlap, loading pulse

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "periphery-v31";
+const CACHE = "periphery-v32";
 const ASSETS = ["./", "./index.html", "./style.css", "./config.js", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
